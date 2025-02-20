0d2f755a42a697fb7bdc489f501b3e288150b559
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-02-20 15:41:41 +0100

    changes to db schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -3,13 +3,21 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     wallet_address VARCHAR NOT NULL,
     user_email VARCHAR NOT NULL,
-    user_twitter VARCHAR NOT NULL,
+    backup_email VARCHAR NOT NULL,
+    passwordHash VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    contact_name VARCHAR NOT NULL,
+    contact_email VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    user_address VARCHAR NOT NULL,
+    user_email VARCHAR NOT NULL,
     token VARCHAR NOT NULL,
     amount VARCHAR NOT NULL,
     transaction_type VARCHAR NOT NULL,
@@ -18,7 +26,7 @@ CREATE TABLE transactions (
 
 CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
-    user_address VARCHAR NOT NULL,
+    user_email VARCHAR NOT NULL,
     message VARCHAR NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
