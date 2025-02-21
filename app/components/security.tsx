37e3dbc76367a9a3d17baacd724b2df9bb5c0115
Afekhide Gbadamosi <realafe21@gmail.com>
2025-02-21 18:48:28 +0100

    Feat: updated font sizing

--- app/components/security.tsx
+++ app/components/security.tsx
@@ -4,11 +4,11 @@ export default function Security() {
     return (
         <section className="py-[41px] md:px-20 px-8 justify-center grid gap-y-[25px]">
             <div className="text-center">
-                <h3 className="font-bold text-3xl">Security & Privacy Commitment</h3>
-                <p className="font-light">Your Security, Our Priority</p>
+                <h3 className="font-bold text-5xl">Security & Privacy Commitment</h3>
+                <p className="font-light text-2xl">Your Security, Our Priority</p>
             </div>
             <Image className="mx-auto" src="/security.png" alt="security" width={388} height={250}/>
-            <ul className="text-lg list-disc">
+            <ul className="md:text-2xl text-lg list-disc">
                 <li>Uses blockchain or other advanced encryption methods.</li>
                 <li>Zero-knowledge transactions—Nebula doesn’t store your data.</li>
                 <li>Secure authentication (e.g., biometric login, 2FA).</li>
