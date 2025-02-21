03326c5099fca3de72092bdb1b9f6f56aa5e3e42
Afekhide Gbadamosi <realafe21@gmail.com>
2025-02-21 18:41:37 +0100

    Feat: updated security component to correct ui

--- app/components/security.tsx
+++ app/components/security.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+
 export default function Security() {
     return (
-        <section
-            className="pt-[120px] pb-[78px] md:px-[112px] px-[20px] text-center relative bg-gradient-to-b from-[#3D46FF] to-[#242A99]">
-            <Image
-                src="/security.png" alt="Security" width={1000} height={1000}
-                   className="absolute top-0 left-0 w-full h-full object-cover"/>
-            <div className="mb-[43px] md:text-4xl text-3xl relative z-10 text-white">
-                <h3 className="font-bold">Reinforce security and ease of use: </h3>
-                <p className="font-light">Start sending secure, private payments today.</p>
+        <section className="py-[41px] md:px-20 px-8 justify-center grid gap-y-[25px]">
+            <div className="text-center">
+                <h3 className="font-bold text-3xl">Security & Privacy Commitment</h3>
+                <p className="font-light">Your Security, Our Priority</p>
             </div>
-            <button className="px-12 appearance-none md:text-xl text-lg py-5 z-10 relative rounded-[31px] bg-white text-[#3D46FF]">Get Started</button>
+            <Image className="mx-auto" src="/security.png" alt="security" width={388} height={250}/>
+            <ul className="text-lg list-disc">
+                <li>Uses blockchain or other advanced encryption methods.</li>
+                <li>Zero-knowledge transactions—Nebula doesn’t store your data.</li>
+                <li>Secure authentication (e.g., biometric login, 2FA).</li>
+            </ul>
         </section>
     );
 }
